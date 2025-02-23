f3d336a5b384a33f206593fe39c742250796e8ba
ohystal <ilya.sid228@mail.ru>
2025-02-23 19:57:41 +0300

    [+] описаны стили для разных состояний задвижки

--- src/components/Gate/Gate.tsx
+++ src/components/Gate/Gate.tsx
@@ -1,8 +1,9 @@
-import style from "./Gate.module.scss";
+import style from"./Gate.module.scss";
 
-export const Gate = (id:string) => {
+export const Gate = () => {
+  
   return (
-    <div className={style.gate} id={id}>
+    <div id={style.open}>
         <svg width="25" height="26" aria-hidden="true">
           <use xlinkHref="/images/sprite.svg#icon-gate-left"></use>
         </svg>
