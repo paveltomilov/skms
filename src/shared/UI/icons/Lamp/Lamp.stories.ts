e9322791cfb02a2cf451cd2d93632ac316ae2e01
Lev Snegov <lev.snegov@bk.ru>
2025-06-19 16:42:26 +0300

    [+]: ModalWrapper, [-]: lamp удалил и объединил в один для удобства,  [*]: добавил стили при вводе в code

---
+++ src/shared/UI/icons/Lamp/Lamp.stories.ts
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Lamp from '.';
+
+
+const meta: Meta<typeof Lamp> = {
+  title: 'Icons/Lamp',
+  component: Lamp,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    className: {
+      description: 'Для передачи дополнительных стилей (для позиционирования)',
+      control: false,
+    },
+    variant: {
+      description: 'Вариант иконки лампы',
+      control: { type: 'select' },
+      options: ['default', 'success', 'error', 'warn'],
+      defaultValue: 'default',
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    variant: 'default',
+  },
+};
+
+export const Success: Story = {
+  args: {
+    variant: 'success',
+  },
+};
+
+export const Error: Story = {
+  args: {
+    variant: 'error',
+  },
+};
+
+export const Warn: Story = {
+  args: {
+    variant: 'warn',
+  },
+};
---
+++ src/widgets/ModalWrapper/index.tsx
@@ -0,0 +1,72 @@
+'use client';
+
+import Button from "@/shared/UI/Button";
+import Input from "@/shared/UI/Input";
+import { FC, useEffect } from "react";
+import styles from './styles.module.scss';
+import Close from "@/shared/UI/icons/Close";
+
+interface ModalProps  {
+	header?: string
+	second?: number
+}
+
+const ModalWrapper: FC<ModalProps & {onClose: () => void; isBlur?: boolean }> = ({
+	header = 'ПКДВ-2',
+	second = 59,
+	onClose,
+	isBlur = false,
+  }) => {
+
+	useEffect(() => {
+		document.body.style.overflow = 'hidden';
+
+		 // Добавим/удалим класс блюра кроме header/footer/sidebar
+		 const blurContainer = document.getElementById('blur-container');
+		if (blurContainer) {
+			if (isBlur) {
+				blurContainer.classList.add(styles.blurBackground)
+			} else {
+				blurContainer.classList.remove(styles.blurBackground)
+			}
+		}
+
+
+		return () => {
+			document.body.style.overflow = ''
+			if (blurContainer) {
+				blurContainer.classList.remove(styles.blurBackground)
+			}
+		}
+	}, [isBlur])
+	const onOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
+		// Если кликнули именно по оверлею
+		if (e.target === e.currentTarget) {
+			onClose()
+		}
+	}
+
+	return (
+	<div className={styles.overlay} onClick={onOverlayClick}>
+      <div className={styles.modal} onClick={e => e.stopPropagation()}>
+        <div className={styles.header}>
+          <p className={styles.headerText}>{header}</p>
+          <button className={styles.button} aria-label="Close" onClick={onClose}>
+            <Close size={"xs"} />
+          </button>
+        </div>
+        <p className={styles.text}>
+          Введите полученный код из 6 символов, которые мы отправили на указанный <br /> Вами e-mail
+        </p>
+        <div className={styles.input}>
+          <Input status='default' type="code" />
+        </div>
+        <p className={styles.textTimer}>Отправить код повторно через {second} сек.</p>
+        <Button id={'1'} height={55} width={270} disabled text='Войти' className={styles.buttonChandge} />
+    </div>
+	</div>
+  );
+};
+
+
+export default ModalWrapper;
