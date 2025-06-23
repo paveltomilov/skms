edbd3fe969cbe06a5911257c17108e3691862a58
Valerii_Panov <val250640@mail.ru>
2025-06-24 08:54:52 +1000

    [+] Добавил AutomatButton

---
+++ src/shared/UI/AutomatButton/index.stories.tsx
@@ -0,0 +1,88 @@
+import {Meta, type StoryObj} from "@storybook/react";
+import {AutomatButton} from "./index";
+import {useState} from "react";
+
+
+const meta: Meta<typeof AutomatButton> = {
+    title: 'UI/AutomatButton',
+    component: AutomatButton,
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+    argTypes: {
+        state: {
+            description: 'Состояние (вкл/выкл)',
+            options: ['on', 'off'],
+            control: {
+                type: 'radio',
+            },
+        },
+        type: {
+            description: 'Тип (закрыто/открыто)',
+            options: ['open', 'close'],
+            control: {
+                type: 'radio',
+            },
+        },
+        disabled: {
+            description: 'Активна/неактивна',
+            options: [true, false],
+            control: {
+                type: 'radio',
+            },
+        },
+        onMouseDown: {
+            description: 'Функция при надавливании',
+        },
+        onMouseUp: {
+            description: 'Функция при отпускании клавиши',
+        },
+    },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+const DemoBtn = ({state = 'off', type = 'close'}) => {
+    const [demoState, setDemoState] = useState(state);
+    const [demoType, setDemoType] = useState(type)
+
+    const demoOnMouseDown = (): void => {
+        setDemoState(oldState => oldState === 'off' ? 'on' : 'off');
+    };
+
+    const demoOnMouseUp = (): void => {
+        setDemoType(oldType => oldType === 'close' ? 'open' : 'close');
+    };
+
+    return <AutomatButton state={demoState as "on" | "off"}
+                          type={demoType as "open" | "close"}
+                          onMouseUp={demoOnMouseUp}
+                          onMouseDown={demoOnMouseDown}/>;
+};
+
+export const Default: Story = {
+    args: {
+        id: 'default-automat-button',
+        name: 'default',
+    },
+    render: () => <DemoBtn/>,
+};
+
+export const Opened: Story = {
+    args: {
+        id: 'opened-automat-button',
+        name: 'opened',
+    },
+    render: () => <DemoBtn state='on' type='open'/>,
+};
+
+export const Closed: Story = {
+    args: {
+        id: 'closed-automat-button',
+        name: 'closed',
+    },
+    render: () => <DemoBtn state='off' type='close'/>,
+};
