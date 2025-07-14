c3f9df0f02eba0a3f0070191b8d6c8b77b496d11
JaneKhris <efkhrisanfova@gmail.com>
2025-07-14 17:25:35 +0300

    [+] добавила headerPtk и window

---
+++ src/shared/UI/Window/index.tsx
@@ -0,0 +1,44 @@
+import styles from './styles.module.scss';
+import { FC } from 'react';
+
+interface Props {
+    color?: 'blue' | 'yellow' ;
+    value?: string;
+    textTop?: string;
+    textBottom?: string;
+    textLeft?: string;
+    textRight?: string;
+    className?: string;
+}
+
+const Window: FC<Props> = ({
+    color,
+    value,
+    textTop,
+    textBottom,
+    textLeft,
+    textRight,
+    className,
+}) => {
+
+    const fieldColor =
+        (color === 'blue') ? `${styles.field_blue}` :
+        (color === 'yellow') ? `${styles.field_yellow}`:
+        `${styles.field_white}`;
+
+    return (
+        <div className={`${styles.window} ${className && className}`}>
+            <div className={`${styles.field} ${fieldColor}`}>
+                {value && <span>{value}</span>}
+            </div>
+            <div className={styles.text}>
+                {textTop && <div className={styles.text_top}>{textTop}</div>}
+                {textBottom && <div className={styles.text_bottom}>{textBottom}</div>}
+                {textLeft && <div className={styles.text_left}>{textLeft}</div>}
+                {textRight && <div className={styles.text_right}>{textRight}</div>}
+            </div>
+        </div>
+    );
+};
+
+export default Window;
--- src/widgets/HeaderPtk/index.tsx
+++ src/widgets/HeaderPtk/index.tsx
@@ -1,11 +1,85 @@
 'use client';
-import { FC } from 'react';
-import style from './styles.module.scss';
+import { FC, useEffect, useState } from 'react';
+import styles from './styles.module.scss';
+import Button from '@/shared/UI/Button';
+import Window from '@/shared/UI/Window';
 
 const HeaderPtk: FC = () => {
+
+	// для вывода показаний частоты и мощности
+
+	const readings = {
+		'freq': 49.96,
+		'pwr': 120.9
+	};
+
+	const [dateTime, setDateTime] = useState(new Date());
+	useEffect(() => {
+		const id = setInterval(() => {
+			setDateTime(new Date());
+		});
+		return () => {
+			clearInterval(id);
+		};
+	}, []);
+
+
 	return (
-		<header className={style.header}>
-			<div className={style.wrapper}>шапка птк</div>
+		<header className={styles.header}>
+			<div className={styles.wrapper}>
+				<div className={styles.windows}>
+					<div className={styles.windows_ptk}>ПТК</div>
+					<div className={styles.windows_defense}>Pабота защит</div>
+					<div className={styles.windows_kpm}>КРМ</div>
+				</div>
+				<Window
+				color = 'yellow'
+				value='-4'
+				textRight='°C'
+				/>
+
+				<div className={styles.buttons}>
+					<Button
+						className={styles.button}
+						width={140}
+						height={32}
+						id="menu"
+						text="Гл. меню"
+						onClick={() => console.log('Нажата кнопка Главное меню')}
+					/>
+					<Button
+						className={styles.button}
+						width={140}
+						height={32}
+						id="signals"
+						text="Сигналы"
+						onClick={() => console.log('Нажата кнопка Сигналы')}
+					/>
+					<Button
+						className={styles.button}
+						width={140}
+						height={32}
+						id="graphs"
+						text="Графики"
+						onClick={() => console.log('Нажата кнопка Графики')}
+					/>
+				</div>
+				<Button
+					className={styles.button}
+					width={32}
+					height={32}
+					id="?"
+					text="?"
+					onClick={() => console.log('Нажата кнопка ?')}
+				/>
+				<div className={styles.datetime}>{dateTime.toLocaleDateString('ru-RU',
+					{
+						year: 'numeric',
+						month: 'long',
+						day: 'numeric',
+					})} {dateTime.toLocaleTimeString('ru-RU')}</div>
+				<div className={styles.readings}>{readings.freq}Гц {readings.pwr}МВт</div>
+			</div>
 		</header>
 	);
 };
