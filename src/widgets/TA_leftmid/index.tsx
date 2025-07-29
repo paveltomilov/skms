04cdc4d1ad5d88dc1a9004923bfcfe2725590abe
Lev Snegov <lev.snegov@bk.ru>
2025-07-29 14:20:12 +0300

    FIX: styles BEM, add icons Rectangle

--- src/widgets/TA_leftmid/index.tsx
+++ src/widgets/TA_leftmid/index.tsx
@@ -3,66 +3,103 @@ import styles from './styles.module.scss';
 import Button from '@/shared/UI/Button';
 import Window from '@/shared/UI/Window';
 import Gate from '@/shared/UI/Gate';
+import classNames from 'classnames';
+import Rectangle from '@/shared/UI/icons/Rectangle';
 
 const TAleftmid: FC = () => {
 	return (
 		<>
 			<div className={styles.container}>
-				<p className={styles.container__p2}>Свежий пар</p>
-				<p className={styles.container__p1__A}>А</p>
-				<div className={styles.container__window__left}>
-					<Window color={'blue'} value={239.7} textRight="т/ч" />
-					<Window color={'blue'} value={544} textRight="°С" />
+				<p className={classNames(styles['container__p2'])}>
+					Свежий пар
+				</p>
+				<p
+					className={classNames(
+						styles['container__p1'],
+						styles['container__p1--modifierA'],
+					)}
+				>
+					А
+				</p>
+
+				<div
+					className={classNames(
+						styles['container__window'],
+						styles['container__window--left'],
+					)}
+				>
+					<Window color="blue" value={239.7} textRight="т/ч" />
+					<Window color="blue" value={544} textRight="°С" />
 				</div>
-				<Gate state={'open'} className={styles.gate} />
-				<p className={styles.container__p3}>ГП3-А</p>
-				<div className={styles.container__window__right}>
-					<Window color={'blue'} value={13} textRight="МПа" />
-					<Window color={'blue'} value={544} textRight="°С" />
+
+				<Gate state="open" className={styles.gate} />
+
+				<p className={styles['container__p3']}>ГП3-А</p>
+
+				<div
+					className={classNames(
+						styles['container__window'],
+						styles['container__window--right'],
+					)}
+				>
+					<Window color="blue" value={13} textRight="МПа" />
+					<Window color="blue" value={544} textRight="°С" />
 				</div>
-				<p className={styles.container__window__right__p}>СКВД-1</p>
+				<p className={styles['container__window-p']}>СКВД-1</p>
 			</div>
-			<div className={styles.container__two}>
-				<p className={styles.container__two__p}>ХПП</p>
-				<div className={styles.container__two__window}>
-					<Window color={'blue'} value={239.7} textRight="МПа" />
-					<Window color={'blue'} value={544} textRight=" °С" />
+			<div className={styles.containerTwo}>
+				<p className={styles['containerTwo__p']}>ХПП</p>
+				<div className={styles['containerTwo__window']}>
+					<Window color="blue" value={239.7} textRight="МПа" />
+					<Window color="blue" value={544} textRight="°С" />
 				</div>
 			</div>
-			<div className={styles.container__three}>
-				<p className={styles.container__three__p}>в котел</p>
+			<div className={styles.containerThree}>
+				<p className={styles['containerThree__p']}>в котел</p>
 				<Button width={88} height={28} text="ПИТ" />
 			</div>
-			<div className={styles.container__four}>
-				<div className={styles.container__four__window}>
-					<Window color={'blue'} value={489.0} textRight="т/ч" />
-					<Window color={'blue'} value={18.3} textRight="МПа" />
-					<Window color={'blue'} value={544} textRight="°С" />
-					<Gate state={'open'} position='vertical' className={styles.container__four__window__gate}/>
-					<p className={styles.container__four__window__p}>1ПВ-5</p>
-				</div>
-				<p className={styles.container__four__p}>КСН</p>
-				<Button width={88} height={28} text="РОУ" />
-				<div className={styles.container__four__window__one}>
-					<Window color={'blue'} value={1.05} textRight="МПа" />
-					<Window color={'blue'} value={242} textRight="°С" />
-				</div>
-				<div className={styles.container__four__window__two}>
-					<Window color={'blue'} value={0} textRight="°С" />
+
+			<div className={styles.containerFour}>
+				<div className={styles['containerFour__window']}>
+					<Window color="blue" value={489.0} textRight="т/ч" />
+					<Window color="blue" value={18.3} textRight="МПа" />
+					<Window color="blue" value={544} textRight="°С" />
+
 					<Gate
-						state={'close'}
-						className={styles.container__four__window__two__gate}
+						state="open"
+						position="vertical"
+						className={styles['containerFour__window-gate']}
+					/>
+
+					<p className={styles['containerFour__window-p']}>1ПВ-5</p>
+				</div>
+
+				<p className={styles['containerFour__p']}>КСН</p>
+				<Button width={88} height={28} text="РОУ" />
+
+				<div className={styles['containerFour__windowOne']}>
+					<Window color="blue" value={1.05} textRight="МПа" />
+					<Window color="blue" value={242} textRight="°С" />
+				</div>
+
+				<div className={styles['containerFour__windowTwo']}>
+					<Window color="blue" value={0} textRight="°С" />
+					<Rectangle
+						color="white"
+						className={styles['containerFour__windowTwo-rectangle']}
 					/>
 				</div>
-				<div className={styles.container__four__window__three}>
-					<p className={styles.container__four__window__three__p2}>
+
+				<div className={styles['containerFour__windowThree']}>
+					<p className={styles['containerFour__windowThree-p2']}>
 						IV отб.
 					</p>
-					<div className={styles.container__four__window__three__w}>
-						<Window color={'blue'} value={0.35} textRight="МПа" />
+
+					<div className={styles['containerFour__windowThree-w']}>
+						<Window color="blue" value={0.35} textRight="МПа" />
 					</div>
 
-					<p className={styles.container__four__window__three__p3}>
+					<p className={styles['containerFour__windowThree-p3']}>
 						ДПВ
 					</p>
 				</div>
