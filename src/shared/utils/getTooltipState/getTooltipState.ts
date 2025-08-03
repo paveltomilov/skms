e9c02af1160438845c77cfb737a64c5ef8e8e59b
JaneKhris <efkhrisanfova@gmail.com>
2025-08-03 18:51:10 +0300

    Reapply "добавила wrapper и stories,"
    This reverts commit f11a13818d8cd861368b391ef7cc9c02be62a46f.

---
+++ src/shared/utils/getTooltipState/getTooltipState.ts
@@ -0,0 +1,32 @@
+import { TooltipState } from '@/shared/types/tooltip';
+
+// функция для получения положения тултипа
+
+export const getTooltipState = (tooltipWidth: number, tooltipHeight: number, target: DOMRect) => {
+    const state: TooltipState = {
+        direction: 'top',
+        side: 'center',
+        positionX: 0,
+        positionTop: null,
+        positionBottom: null,
+    };
+
+    if (target.bottom > window.innerHeight - tooltipHeight) {
+        state.direction = 'bottom';
+        state.positionBottom = window.innerHeight - target.y + 8.5;
+    } else {
+        state.positionTop = target.bottom + 8.5;
+    }
+    if (target.x < tooltipWidth / 2) {
+        state.side = 'left';
+        state.positionX = target.x + (target.right - target.x) / 2;
+    } else {
+        if (target.right > (window.innerWidth - tooltipWidth / 2)) {
+            state.side = 'right';
+            state.positionX = target.x + (target.right - target.x) / 2 - tooltipWidth;
+        } else {
+            state.positionX = target.x + (target.right - target.x) / 2 - tooltipWidth / 2;
+        }
+    }
+    return state;
+};
