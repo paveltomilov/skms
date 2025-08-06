70e956b4565dc2fa7176c787b1ea6075dd6505ba
VladimirBikaev <bikaew01.93@yandex.ru>
2025-08-06 23:53:34 +0200

    TA_rightTop

--- src/_pages/turbine-unit/index.tsx
+++ src/_pages/turbine-unit/index.tsx
@@ -1,5 +1,6 @@
 import styles from './styles.module.scss';
 import TALeftTop from '@/widgets/TA_lefftop';
+import TARrightTop from '@/widgets/TA_rightop';
 
 const TurbineUnitPage = () => {
 	return (
@@ -14,7 +15,7 @@ const TurbineUnitPage = () => {
 				<div className={styles.section__content_mid_mid}></div>
 				<div className={styles.section__content_mid_down}></div>
 
-				<div className={styles.section__content_right_top}></div>
+				<TARrightTop className={styles.section__content_right_top} />
 				<div className={styles.section__content_right_mid}></div>
 				<div className={styles.section__content_right_down}></div>
 			</div>
---
+++ src/widgets/TA_rightop/index.tsx
@@ -0,0 +1,72 @@
+import { FC } from 'react';
+import cn from 'classnames';
+import styles from './styles.module.scss';
+import Tilde from '@/shared/UI/icons/Tilde';
+import Window from '@/shared/UI/Window';
+import { WINDOWS } from '@/shared/configs/window';
+
+interface Props {
+	className?: string;
+}
+
+const TARrightTop: FC<Props> = ({ className }) => {
+	return (
+		<div className={cn(className, styles.container)}>
+			<div className={styles.blockCND}>
+				<Window
+					color="blue"
+					value={WINDOWS.w68.currentValue}
+					textRight={WINDOWS.w68.unitsMeasurement}
+					colorText="white"
+				/>
+				<Window
+					color="blue"
+					value={WINDOWS.w69.currentValue}
+					textRight={WINDOWS.w69.unitsMeasurement}
+					colorText="white"
+				/>
+				<p className={styles.blockCND__center}>ЦНД</p>
+				<Window
+					color="blue"
+					value={WINDOWS.w72.currentValue}
+					textRight={WINDOWS.w72.unitsMeasurement}
+					colorText="white"
+				/>
+				<Window
+					color="blue"
+					value={WINDOWS.w73.currentValue}
+					textRight={WINDOWS.w73.unitsMeasurement}
+					colorText="white"
+				/>
+				<div className={styles.blockCND__center}>
+					<Window
+						color="blue"
+						value={WINDOWS.w74.currentValue}
+						textRight={WINDOWS.w74.unitsMeasurement}
+
+					/>
+				</div>
+			</div>
+			<div className={styles.blockGenerator}>
+				<div className={styles.blockGenerator__left}>
+					<Window
+						color="blue"
+						value={WINDOWS.w70.currentValue}
+						textRight={WINDOWS.w70.unitsMeasurement}
+					/>
+					<Tilde size="md" />
+					<p>Генератор</p>
+				</div>
+				<div className={styles.blockGenerator__windowAmper}>
+					<Window
+						color="blue"
+						value={WINDOWS.w71.currentValue}
+						textRight={WINDOWS.w71.unitsMeasurement}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default TARrightTop;
