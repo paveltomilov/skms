a8bcf4862f6de851da86468f6460953b895df8d9
Lev Snegov <lev.snegov@bk.ru>
2025-08-22 13:32:21 +0300

    [*]: сделал рефакторинг элемента, сократил код, сделал все, как в макете под PP

--- src/widgets/KA_leftmid/index.tsx
+++ src/widgets/KA_leftmid/index.tsx
@@ -10,184 +10,97 @@ interface Props {
 	className?: string;
 }
 
+type TildaConfig = {
+	color?: 'white' | 'green'
+	disabled?: boolean
+}
+
+const firstWindows = [WINDOWS.w113, WINDOWS.w114, WINDOWS.w115, WINDOWS.w116, WINDOWS.w117, WINDOWS.w118];
+const secondWindows = [WINDOWS.w119, WINDOWS.w120, WINDOWS.w121, WINDOWS.w122, WINDOWS.w123, WINDOWS.w124];
+const thirdWindows = [WINDOWS.w125, WINDOWS.w126, WINDOWS.w127, WINDOWS.w128, WINDOWS.w129, WINDOWS.w130];
+const fourthWindows = [WINDOWS.w131, WINDOWS.w132, WINDOWS.w133, WINDOWS.w134, WINDOWS.w135, WINDOWS.w136];
+
+const buttonsConfig = [
+	{ text: 'РЗМ 1МВ-А', bgStyle: styles.btn__bgWhite },
+	{ text: 'РЗМ 1МВ-Б', bgStyle: styles.btn__bgGreen },
+	{ text: 'РЗМ 1МВ-В', bgStyle: styles.btn__bgGreen },
+	{ text: 'РЗМ 1МВ-Г', bgStyle: styles.btn__bgGreen },
+	{ text: 'РЗМ 1МВ-Д', bgStyle: styles.btn__bgWhite },
+	{ text: 'РЗМ 1МВ-Е', bgStyle: styles.btn__bgWhite },
+];
+
+const tildaConfig: TildaConfig[] = [
+	{ color: 'white', disabled: true },
+	{},
+	{},
+	{},
+	{},
+	{ color: 'white' }
+];
+
 const KALeftMid: FC<Props> = ({ className }) => {
 	return (
 		<div className={cn(className, styles.container)}>
 			<div className={styles.windows}>
-				<Window
-					color="blue"
-					value={WINDOWS.w113.currentValue}
-					textRight={WINDOWS.w113.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w114.currentValue}
-					textRight={WINDOWS.w114.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w115.currentValue}
-					textRight={WINDOWS.w115.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w116.currentValue}
-					textRight={WINDOWS.w116.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w117.currentValue}
-					textRight={WINDOWS.w117.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w118.currentValue}
-					textRight={WINDOWS.w118.unitsMeasurement}
-				/>
+				{firstWindows.map((window, index) => (
+					<Window
+						key={index}
+						color="blue"
+						value={window.currentValue}
+						textRight={window.unitsMeasurement}
+					/>))}
 			</div>
-
 			<div className={styles.windows__button}>
-				<Button
-					width={74}
-					height={16}
-					text="РЗМ 1МВ-А"
-					className={cn(styles.btn, styles.btn__bgWhite)}
-				/>
-				<Button
-					width={74}
-					height={16}
-					text="РЗМ 1МВ-Б"
-					className={cn(styles.btn, styles.btn__bgWhite)}
-
-				/>
-				<Button
-					width={74}
-					height={16}
-					text="РЗМ 1МВ-В"
-					className={cn(styles.btn, styles.btn__bgGreen)}
-				/>
-				<Button
-					width={74}
-					height={16}
-					text="РЗМ 1МВ-Г"
-					className={cn(styles.btn, styles.btn__bgGreen)}
-				/>
-				<Button
-					width={74}
-					height={16}
-					text="РЗМ 1МВ-Д"
-					className={cn(styles.btn, styles.btn__bgGreen)}
-				/>
-				<Button
-					width={74}
-					height={16}
-					text="РЗМ 1МВ-Е"
-					className={cn(styles.btn, styles.btn__bgWhite)}
-				/>
+				{buttonsConfig.map((btn, index) => (
+					<Button
+						key={index}
+						width={74}
+						height={16}
+						text={btn.text}
+						className={cn(styles.btn, btn.bgStyle)}
+					/>
+				))}
 			</div>
 			<div className={styles.windows__tilda}>
-				<Tilde size='md' disable color='white' />
-				<Tilde size='md' />
-				<Tilde size='md' />
-				<Tilde size='md' />
-				<Tilde size='md' />
-				<Tilde size='md' color='white' />
+				{tildaConfig.map((tilda, index) => (
+					<Tilde
+						key={index}
+						size='md'
+						color={tilda.color}
+						disable={tilda.disabled}
+					/>
+				))}
 			</div>
 			<div className={styles.windows}>
-				<Window
-					color="blue"
-					value={WINDOWS.w119.currentValue}
-					textRight={WINDOWS.w119.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w120.currentValue}
-					textRight={WINDOWS.w120.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w121.currentValue}
-					textRight={WINDOWS.w121.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w122.currentValue}
-					textRight={WINDOWS.w122.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w123.currentValue}
-					textRight={WINDOWS.w123.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w124.currentValue}
-					textRight={WINDOWS.w124.unitsMeasurement}
-				/>
+				{secondWindows.map((window, index) => (
+					<Window
+						key={index}
+						color="blue"
+						value={window.currentValue}
+						textRight={window.unitsMeasurement}
+					/>
+				))}
+			</div>
+
+			<div className={styles.windows}>
+				{thirdWindows.map((window, index) => (
+					<Window
+						key={index}
+						color="blue"
+						value={window.currentValue}
+						textRight={window.unitsMeasurement}
+					/>
+				))}
 			</div>
 			<div className={styles.windows}>
-				<Window
-					color="blue"
-					value={WINDOWS.w125.currentValue}
-					textRight={WINDOWS.w125.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w126.currentValue}
-					textRight={WINDOWS.w126.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w127.currentValue}
-					textRight={WINDOWS.w127.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w128.currentValue}
-					textRight={WINDOWS.w128.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w129.currentValue}
-					textRight={WINDOWS.w129.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w130.currentValue}
-					textRight={WINDOWS.w130.unitsMeasurement}
-				/>
-			</div>
-			<div className={styles.windows}>
-				<Window
-					color="blue"
-					value={WINDOWS.w131.currentValue}
-					textRight={WINDOWS.w131.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w132.currentValue}
-					textRight={WINDOWS.w132.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w133.currentValue}
-					textRight={WINDOWS.w133.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w134.currentValue}
-					textRight={WINDOWS.w134.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w135.currentValue}
-					textRight={WINDOWS.w135.unitsMeasurement}
-				/>
-				<Window
-					color="blue"
-					value={WINDOWS.w136.currentValue}
-					textRight={WINDOWS.w136.unitsMeasurement}
-				/>
+				{fourthWindows.map((window, index) => (
+					<Window
+						key={index}
+						color="blue"
+						value={window.currentValue}
+						textRight={window.unitsMeasurement}
+					/>
+				))}
 			</div>
 		</div>
 	);
