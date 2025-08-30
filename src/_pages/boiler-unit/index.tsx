ecaf9337bf5f1b73ca2748313b50cc09ba414d22
VladimirBikaev <bikaew01.93@yandex.ru>
2025-08-30 19:06:25 +0200

    update branch

--- src/_pages/boiler-unit/index.tsx
+++ src/_pages/boiler-unit/index.tsx
@@ -7,13 +7,15 @@ import KALeftMid from '@/widgets/KA_leftmid';
 import KALeftDown from '@/widgets/KA_leftdown';
 import KARightMid from '@/widgets/KA_rightmid';
 import KAMidMid from '@/widgets/KA_midmid';
+import KALeftTop from '@/widgets/KA_lefttop';
+import KARightTop from '@/widgets/KA_righttop';
 
 const BoilerUnitPage = () => {
 	return (
 		<section className={styles.section}>
 			<h1 className={styles.section__title}>КА - Котлоагрегат</h1>
 			<div className={styles.section__content}>
-				<KALeftTop className={styles.section__content_left_top}/>
+				<KALeftTop className={styles.section__content_left_top} />
 				<KALeftMid className={styles.section__content_left_mid} />
 				<KALeftDown className={styles.section__content_left_down} />
 
