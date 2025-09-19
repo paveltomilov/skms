acb3b4962b7ad73a13328a0985de3949cd193fed
Pavel-Tomilov <pasha.tomiloff@gmail.com>
2025-09-19 15:37:48 +0300

    создал новую ветку

---
+++ src/widgets/PopupClamp/index.tsx
@@ -0,0 +1,17 @@
+import Channel from '@/shared/UI/icons/Channel';
+import Screw from '@/shared/UI/icons/Screw';
+import Marker from '@/shared/UI/Marker';
+import Provod from '@/shared/UI/Provod';
+
+const PopupClamp = () => {
+	return (
+		<div>
+			<Provod />
+			<Marker className="" text="A" />
+			<Screw />
+			<Channel />
+		</div>
+	);
+};
+
+export default PopupClamp;
