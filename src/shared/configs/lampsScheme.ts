3a026f9a782bdcc58c8e87410ec719fb1f142ffe
VladimirBikaev <bikaew01.93@yandex.ru>
2025-09-22 22:55:56 +0200

    внес правки указанные выше

--- src/shared/configs/lampsScheme.ts
+++ src/shared/configs/lampsScheme.ts
@@ -1,8 +1,10 @@
-export const pins = [
+import { MarkerName } from '../types/markers';
+
+export const pins: { code: MarkerName }[] = [
 	{ code: 'A' as const },
 	{ code: 'N' as const },
 ];
 export const columns = [
-		{ title: 'Закрыто', color: 'white' as const },
-		{ title: 'Открыто', color: 'lamp_green' as const },
-	];
+	{ title: 'Закрыто', color: 'white' as const },
+	{ title: 'Открыто', color: 'lamp_green' as const },
+];
--- src/widgets/LampScheme/index.tsx
+++ src/widgets/LampScheme/index.tsx
@@ -16,18 +16,41 @@ export const LampScheme: FC = () => {
 			{columns.map(({ title, color }) => (
 				<section
 					key={title}
-					className={cn(styles.column, title === 'Открыто' && styles.columnRight)}
+					className={cn(styles.column, {
+						[styles.columnRight]: title === 'Открыто',
+					})}
 					aria-labelledby={`lamp-${title}`}
 				>
-					<h2 id={`lamp-${title}`} className={styles.title}>{title}</h2>
+					<h3 className={styles.title} id={`lamp-${title}`} >
+						{title}
+					</h3>
 					<LampIndicator color={color} aria-hidden />
 					<ul className={styles.foot} aria-label="Клеммы">
 						{pins.map(({ code }) => (
-							<li key={code} className={styles.pin} aria-label={`Клемма ${code}`}>
-								<Channel size="md" className={styles.pin__channel} aria-hidden />
-								<Screw className={styles.pin__screw} textLeft={code} aria-hidden />
-								<ProvodLine length={110} className={styles.pin__pref} aria-hidden />
-								<Marker text={code} className={styles.pin__marker} />
+							<li
+								key={code}
+								className={styles.pin}
+								aria-label={`Клемма ${code}`}
+							>
+								<Channel
+									size="md"
+									className={styles.pin__channel}
+									aria-hidden
+								/>
+								<Screw
+									className={styles.pin__screw}
+									//textLeft={code} после MR поправим 
+									aria-hidden
+								/>
+								<ProvodLine
+									length={110}
+									className={styles.pin__pref}
+									aria-hidden
+								/>
+								<Marker
+									text={code}
+									className={styles.pin__marker}
+								/>
 							</li>
 						))}
 					</ul>
