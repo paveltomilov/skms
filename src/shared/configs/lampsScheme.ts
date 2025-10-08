e8c8bd9c2d98c53b838a19508b7859759b50d5c0
JaneKhris <efkhrisanfova@gmail.com>
2025-10-08 14:33:19 +0300

    убрала изменения в лампочках

--- src/shared/configs/lampsScheme.ts
+++ src/shared/configs/lampsScheme.ts
@@ -4,22 +4,7 @@ export const pins: { code: MarkerName }[] = [
 	{ code: 'A' as const },
 	{ code: 'N' as const },
 ];
-
-interface Column {
-	title: 'Закрыто' | 'Открыто',
-	color: 'white' | 'red' | 'lamp_green',
-	point: string
-};
-
-export const columns: Column[] = [
-	{ 
-		title: 'Закрыто', 
-		color: 'white' as const ,
-		point: 'p.c.3.1.2'
-	},
-	{ 
-		title: 'Открыто', 
-		color: 'lamp_green' as const , 
-		point: 'p.c.3.2.2'
-	},
+export const columns = [
+	{ title: 'Закрыто', color: 'white' as const },
+	{ title: 'Открыто', color: 'lamp_green' as const },
 ];
--- src/widgets/LampScheme/index.tsx
+++ src/widgets/LampScheme/index.tsx
@@ -13,7 +13,7 @@ import ProvodLine from '@/shared/UI/icons/ProvodLine';
 export const LampScheme: FC = () => {
 	return (
 		<section className={styles.container} aria-label="Схема ламп">
-			{columns.map(({ title, color , point }) => (
+			{columns.map(({ title, color }) => (
 				<section
 					key={title}
 					className={cn(styles.column, {
@@ -38,7 +38,7 @@ export const LampScheme: FC = () => {
 									aria-hidden
 								/>
 								<ScrewConnection
-									pointId={code === 'A' ? point : 'p.c.n'}
+									pointId='unknown'
 									className={styles.pin__screw}
 									textLeft={code}
 									aria-hidden
