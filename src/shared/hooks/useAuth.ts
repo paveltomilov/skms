227a35a41f4b833b4fe64448d32e975459678f01
Vyacheslav Agapov <andreevich1992@list.ru>
2025-10-12 19:02:11 +0300

    исправил замечания от 08.10.25 в задаче 123

--- src/shared/hooks/useAuth.ts
+++ src/shared/hooks/useAuth.ts
@@ -3,7 +3,7 @@ import { useEffect, useState, useCallback } from 'react';
 import axios, { AxiosError } from 'axios';
 import { jwtDecode } from 'jwt-decode';
 import { getCookie } from 'cookies-next';
-import { checkAuth, logout } from '@/shared/lib/auth'; // 🔧 ИСПОЛЬЗУЕМ СУЩЕСТВУЮЩИЕ ФУНКЦИИ
+import { checkAuth, logout } from '@/shared/lib/auth'; 
 
 const urlBase: string | undefined = process.env.NEXT_PUBLIC_API_BASE_URL;
 
