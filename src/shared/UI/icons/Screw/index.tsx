6f7faa9a85b3a429fa8c85ebffbf29c23ea06c32
VladimirBikaev <bikaew01.93@yandex.ru>
2025-10-12 23:26:11 +0200

    правка lint

--- src/shared/UI/icons/Screw/index.tsx
+++ src/shared/UI/icons/Screw/index.tsx
@@ -15,7 +15,7 @@ interface Props {
 
 const Screw: FC<Props> = ({
 	className,
-	status = "close",
+	status = 'close',
 	textRight,
 	textTop,
 	textLeft,
@@ -32,7 +32,9 @@ const Screw: FC<Props> = ({
 				width={sizes.width}
 				height={sizes.height}
 				viewBox={sizes.viewBox}
-				className={cn(styles.icon, { [styles.icon_open]: status === "open" })}
+				className={cn(styles.icon, {
+					[styles.icon_open]: status === 'open',
+				})}
 			>
 				<use
 					xlinkHref="/svg/sprite.svg#screw"
