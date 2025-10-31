d8db3c5ead1bfd3e18b8469ee354072d0f6dc2b2
Vyacheslav Agapov <andreevich1992@list.ru>
2025-10-31 20:38:36 +0300

    [+] дополнили warning при регистрации пользователя

--- src/shared/configs/login.ts
+++ src/shared/configs/login.ts
@@ -53,7 +53,7 @@ export const config: Login = [
 		required: true,
 		errorMessage: 'Имя введено неверно',
 		warnMessage:
-			'Имя не должно содержать в себе символы @, #, ! и также кирилицу',
+			'Поле может содержать только буквы латиницы (но не их смешение), пробел, тире и цифры',
 		validate: (state: LoginFormData) => {
 			if (!state.first_name.trim()) return 0;
 
@@ -69,7 +69,7 @@ export const config: Login = [
 		required: true,
 		errorMessage: 'Фамилия введена неверно',
 		warnMessage:
-			'Фамилия не должна содержать в себе символы @, #, ! и также кирилицу',
+			'Поле может содержать только буквы латиницы (но не их смешение), пробел, тире и цифры',
 		validate: (state: LoginFormData) => {
 			if (!state.last_name.trim()) return 0;
 
