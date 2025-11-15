3a9107f1be5e72a9a4335bc9398aab66011580ff
Vyacheslav Agapov <andreevich1992@list.ru>
2025-11-15 21:38:58 +0300

    [+] восстановил лэндинг

---
+++ src/componentsLanding/Carousel/index.tsx
@@ -0,0 +1,62 @@
+import { FC, useState, useEffect, useRef } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import styles from './styles.module.scss';
+
+import type { Swiper as SwiperCore } from 'swiper';
+import ArrowLeftIcon from '../IconSvg/arrowLeft';
+import ArrowRightIcon from '../IconSvg/arrowRight';
+
+const Carousel: FC<{ slides: React.ReactNode[] }> = ({ slides }) => {
+    const [swiperInstance, setSwiperInstance] = useState<SwiperCore | null>(
+        null,
+    );
+
+    const swiperRef = useRef<SwiperCore | null>(null);
+
+    useEffect(() => {
+        if (swiperInstance) {
+            swiperRef.current = swiperInstance;
+        }
+    }, [swiperInstance]);
+
+    return (
+        <div className={styles.carousel}>
+            <Swiper
+                modules={[Autoplay]}
+                spaceBetween={20}
+                slidesPerView={2}
+                slidesPerGroup={2}
+                loop
+                autoplay={{
+                    delay: 50000,
+                    disableOnInteraction: false,
+                }}
+                onSwiper={setSwiperInstance}
+                className={styles.swiper}
+            >
+                {slides.map((content, i) => (
+                    <SwiperSlide key={i} className={styles.slide}>
+                        {content}
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+
+            <button className={styles.swiper__button}>
+                <ArrowLeftIcon
+                    aria-label="Предыдущий слайд"
+                    onClick={() => swiperRef.current?.slidePrev()}
+                    className={`${styles.arrow} ${styles.prev}`}
+                />
+                <ArrowRightIcon
+                    aria-label="Следующий слайд"
+                    onClick={() => swiperRef.current?.slideNext()}
+                    className={`${styles.arrow} ${styles.next}`}
+                />
+            </button>
+        </div>
+    );
+};
+
+export default Carousel;
---
+++ src/componentsLanding/HearoLanding/index.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react';
+import styles from './styles.module.scss'; // ваш SCSS‑файл
+import HeroTitle from './HeroTitle';
+import HeroDescr from './HeroDescr';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({
+    weight: ['400', '500', '700'],
+    subsets: ['latin'],
+});
+
+const HeroLanding: FC = () => {
+    const titleLines: [string, string, string] = [
+        'Skill ',
+        'Management ',
+        'System ',
+    ];
+
+    return (
+        <section id="hero" className={styles.hero}>
+            <div className={`${styles.hero__container} container`}>
+                <div className={`${styles.hero__title} ${inter.className}`}>
+                    <HeroTitle titleLines={titleLines} />
+                </div>
+                <div className={styles.hero__content}>
+                    <HeroDescr />
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default HeroLanding;
