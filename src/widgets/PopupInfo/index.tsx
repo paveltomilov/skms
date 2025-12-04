14f45dbb46d879a6544fc4bb343a675514bc98e2
JaneKhris <efkhrisanfova@gmail.com>
2025-12-04 20:35:15 +0300

    правка верстки

--- src/widgets/PopupInfo/index.tsx
+++ src/widgets/PopupInfo/index.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react';
 import styles from './styles.module.scss';
+import Error from '@/shared/UI/icons/Error';
 
 export const PopupInfo: FC<{ content: 'start' | 'current' | 'malfunctions' }> = ({ content }) => {
 
@@ -11,6 +12,7 @@ export const PopupInfo: FC<{ content: 'start' | 'current' | 'malfunctions' }> = 
                 <div className={styles.popup__content}>Симуляция уже активна. Завершите текущую перед началом новой.</div>}
             {content == 'malfunctions' &&
                 <div className={styles.popup__content__malfunctions}>
+                    <Error className={styles.popup__content__malfunctions__img}/>
                     <div className={styles.popup__content__malfunctions__text}>Не все дефекты найдены. Попробуйте еще раз!</div>
                 </div>}
         </div>
