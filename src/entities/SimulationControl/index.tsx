d00325a1e1ce12746a753c320f50379bbb95d7a5
JaneKhris <efkhrisanfova@gmail.com>
2025-12-10 19:47:58 +0300

    правки по замечаниям

--- src/entities/SimulationControl/index.tsx
+++ src/entities/SimulationControl/index.tsx
@@ -6,23 +6,26 @@ import Button from '@/shared/UI/Button';
 import { openModal } from '@/store/modalSlice';
 import Timer from '../Timer';
 
-const SimulationAttributes: FC<{ className?: string }> = ({ className }) => {
+const SimulationControl: FC<{ className?: string }> = ({ className }) => {
     const dispatch = useDispatch();
+
+    const openUnfinished = () => {
+        dispatch(openModal('infoUnfinished'))
+    }
+
     return (
-        <div className={cn(styles.attributes, className)}>
+        <div className={cn(styles.control, className)}>
             <Button
                 width={89}
                 height={38}
                 aria-label={'Задвижка исправна'}
                 text={'Задвижка исправна'}
-                className={styles.button__finish}
-                onClick={() => dispatch(openModal('infoUnfinished'))}
+                className={styles.control__buttonFinish}
+                onClick={openUnfinished}
             />
-            <div className={styles.line}></div>
             <Timer/>
         </div>
-
     );
 };
 
-export default SimulationAttributes;
+export default SimulationControl;
--- src/widgets/PopupInfo/index.tsx
+++ src/widgets/PopupInfo/index.tsx
@@ -4,17 +4,31 @@ import Error from '@/shared/UI/icons/Error';
 
 export const PopupInfo: FC<{ content: 'start' | 'current' | 'malfunctions' }> = ({ content }) => {
 
+    const renderContent = () => {
+        switch (content) {
+            case 'start':
+                return (
+                    <h2 className={styles.popup__content}>Получена новая неисправность. Симуляция запущена.</h2>
+                );
+            case 'current':
+                return (
+                    <h2 className={styles.popup__content}>Симуляция уже активна. Завершите текущую перед началом новой.</h2>
+                );
+            case 'malfunctions':
+                return (
+                    <div className={styles.popup__content__malfunctions}>
+                        <Error className={styles.popup__content__malfunctions__img} />
+                        <h2 className={styles.popup__content__malfunctions__text}>Не все дефекты найдены. Попробуйте еще раз!</h2>
+                    </div>
+                );
+            default:
+                return <h1>Hello!</h1>;
+        }
+    };
+
     return (
         <div className={styles.popup}>
-            {content === 'start' &&
-                <div className={styles.popup__content}>Получена новая неисправность. Симуляция запущена.</div>}
-            {content == 'current' &&
-                <div className={styles.popup__content}>Симуляция уже активна. Завершите текущую перед началом новой.</div>}
-            {content == 'malfunctions' &&
-                <div className={styles.popup__content__malfunctions}>
-                    <Error className={styles.popup__content__malfunctions__img}/>
-                    <div className={styles.popup__content__malfunctions__text}>Не все дефекты найдены. Попробуйте еще раз!</div>
-                </div>}
+            {renderContent()}
         </div>
     );
 };
