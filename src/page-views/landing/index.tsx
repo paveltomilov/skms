dde7e5bc3ba8d4577e78bd573fdcb7f2bab89ffd
Pavel-Tomilov <pasha.tomiloff@gmail.com>
2026-01-13 18:53:52 +0300

    добавил исключения

--- src/page-views/landing/index.tsx
+++ src/page-views/landing/index.tsx
@@ -1,15 +1,15 @@
-import About from '@/widgets/landing/About';
-import Advantages from '@/widgets/landing/Advantages';
-import FormSection from '@/widgets/landing/FormSection';
-import ForWhom from '@/widgets/landing/ForWhom';
-import Header from '@/widgets/landing/HeaderLanding';
-import HeroLanding from '@/widgets/landing/HearoLanding';
-import Product from '@/widgets/landing/Product';
-import Reviews from '@/widgets/landing/Reviews';
-import Survey from '@/widgets/landing/Survey';
+// import About from '@/widgets/landing/About';
+// import Advantages from '@/widgets/landing/Advantages';
+// import FormSection from '@/widgets/landing/FormSection';
+// import ForWhom from '@/widgets/landing/ForWhom';
+// import Header from '@/widgets/landing/HeaderLanding';
+// import HeroLanding from '@/widgets/landing/HearoLanding';
+// import Product from '@/widgets/landing/Product';
+// import Reviews from '@/widgets/landing/Reviews';
+// import Survey from '@/widgets/landing/Survey';
 import './styles/globals-landing.scss';
 import { FC } from 'react';
-import Footer from '@/widgets/landing/Footer';
+// import Footer from '@/widgets/landing/Footer';
 import CheckboxGroup from '@/widgets/landing/Survey1280';
 
 const LandingPage: FC = () => {
