d9d8f3c0c970644e0739c96354378b8225cc43fd
Pavel-Tomilov <pasha.tomiloff@gmail.com>
2026-01-14 23:05:48 +0300

    сверстал первую страницу

--- src/widgets/landing/SurveyApp/index.tsx
+++ src/widgets/landing/SurveyApp/index.tsx
@@ -1,17 +1,42 @@
 // SurveyApp.tsx - упрощенная версия
 'use client';
 
-import React from 'react';
-
-import styles from './styles.module.scss';
+import React, { useState } from 'react';
+import Image from 'next/image';
 import TopCircle from '../IconSvg/topCircle';
 import BottomCircle from '../IconSvg/bottomCircle';
+import Button from '../Button';
+import ConsentCheckbox from '../ConsentCheckbox';
+import styles from './styles.module.scss';
 
 const SurveyApp: React.FC = () => {
+	const [consent, setConsent] = useState(false);
+
 	return (
-		<div className={styles.survey__container}>
-			<TopCircle className={styles.svg1} />
-			<BottomCircle className={styles.svg2} />
+		<div className={`${styles.survey__container} container`}>
+			<TopCircle className={styles.survey__svg1} />
+			<Image
+				className={styles.survey__img}
+				src="/images/survey_img.png"
+				alt="Картинка"
+				width={512}
+				height={223}
+			/>
+			<h2 className={styles.survey__title}>
+				Помогите нам сделать обучение лучше
+			</h2>
+			<p className={styles.survey__description}>
+				Вместе создадим сильный и удобный симулятор
+			</p>
+			<Button
+				className={styles.survey__button}
+				radius={4}
+				width={515}
+				height={48}
+				text="Начать"
+			/>
+			<ConsentCheckbox value={consent} onChange={setConsent} />
+			<BottomCircle className={styles.survey__svg2} />
 		</div>
 	);
 };
